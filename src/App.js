7776669b276e51164b5e101aa2c00044f5cabb10
danilo <danilodiez1@hotmail.com>
2020-06-06 17:18:54 -0300

    Agregado de imagen y titulo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import Imagen from './components/Imagen'
 
 function App() {
   return (
     <div className="App">
-
+      <div className="title">
+        <h1>May the force be with you</h1>
+      </div>
+      <Imagen className="gif-centro"/>
       
     </div>
   );
---
+++ src/components/Imagen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import gif from './assets/14596.gif'
+
+
+const Imagen = () => {
+    return ( 
+        <img src={gif}></img>
+
+
+     );
+}
+ 
+export default Imagen;
