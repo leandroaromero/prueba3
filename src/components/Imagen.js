3ec3be82e72f81c9ade3a857a4ab4971ec060e6c
danilodiez <danilodiez1@hotmail.com>
2020-06-07 18:35:55 -0300

    modificacion de archivo de config cloud

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -4,11 +4,7 @@ import gif from './assets/14596.gif'
 
 const Imagen = () => {
     return ( 
-<<<<<<< HEAD
         <img src={gif} alt="Logo de la aplicacion"></img>
-=======
-        <img src={gif} alt="Gif de Star Wars"></img>
->>>>>>> develop
 
 
      );
