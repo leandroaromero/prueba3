bf71a64d3c874f1447bdb650cc2e591f144bbc17
danilodiez <danilodiez1@hotmail.com>
2020-06-14 18:21:03 -0300

    Cambio de pruebas

--- __tests__/producto.test.js
+++ __tests__/producto.test.js
@@ -1,5 +1,5 @@
 const producto = require('../src/functions/producto');
 
-test('Resta de 1 y 2 = 2', () => {
-  expect(producto(1, 2)).toBe(-1);
+test('Producto de 1 y 2 = 2', () => {
+  expect(producto(1, 2)).toBe(2);
 });
