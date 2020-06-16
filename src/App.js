e36c44b6ca81a745cc28230002fa1aab9876b3f8
danilodiez <danilodiez1@hotmail.com>
2020-06-16 14:09:21 -0300

    nueva palabra

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <div className="title">
         <h1>May the force be with you</h1>
+        <h3>Hola</h3>
       </div>
       <Imagen className="gif-centro"/>
       <Calculadora/>
