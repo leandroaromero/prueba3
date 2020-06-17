74a4f852adbdcb92370ade0c72330820f41af549
Leandro Antonio Romero <leandroaromero@yahoo.com.ar>
2020-06-16 23:22:10 -0300

    clock

---
+++ src/.babelrc.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  "plugins": [
+    [
+      "@babel/plugin-proposal-class-properties",
+      {
+        "loose": true
+      }
+    ]
+  ]
+};
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: ['@babel/plugin-proposal-class-properties']
+};
